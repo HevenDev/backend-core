b9ae28e6b19ccf491982c892a14940453d533b0f
chandraprakash-ensue <chandra.prakash@ensuesoft.com>
2026-01-19 23:48:42 +0530

    cloudinary added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import cookieParser from "cookie-parser";
 import express from "express";
-
+import cors from 'cors';
 const app = express();
 
 app.use(
@@ -11,7 +11,7 @@ app.use(
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static, "public");
+app.use(express.static("public"));
 app.use(cookieParser());
 
 export default app;
