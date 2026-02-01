dca313dca84d2b337aeedf733f97f2ce2a4be74d
Chandra Prakash <chandra.prakash@ensuesoft.com>
2026-02-02 00:59:35 +0530

    multer setup successfully

--- src/db/index.js
+++ src/db/index.js
@@ -4,13 +4,12 @@ import { DB_NAME } from "../constants.js";
 export const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`
+      process.env.MONGODB_URI,
+      {
+        dbName: DB_NAME  
+      }
     );
-    console.log(
-      "MongoDb connection success",
-      "DB host",
-      connectionInstance.connection.host
-    );
+    console.log("MongoDb connection success", "DB host", connectionInstance.connection.host);
   } catch (error) {
     console.error("MONGO DB Connection failed: ", error);
   }
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,7 +1,24 @@
-import {v2 as cloudinary} from cloudinary;
+import { v2 as cloudinary } from cloudinary;
+import fs from 'fs';
 
-cloudinary.config({ 
-        cloud_name: process.env.di0n06cew, 
-        api_key: process.env.CLOUDINARY_API_KEY, 
-        api_secret: process.env.CLOUDINARY_SECRET_KEY 
-    });
+const uploadFileOnCloudinary = async (localFileUplaod) => {
+    try {
+        if (!localFileUplaod) return null;
+        // upload file on cloudinary
+        const response = await cloudinary.uploader.upload(localFileUplaod, { resource_type: "auto" })
+        //file has been uploaded successfully
+        console.log("File is uploaded on cloudinary", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFileUplaod) // remove the local uploaded temp file from the server
+        return null;
+    }
+}
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+export { uploadFileOnCloudinary }
