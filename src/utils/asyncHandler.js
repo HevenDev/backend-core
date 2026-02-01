f881fe9b7fe1e09b36228e402258165acebd9071
Chandra Prakash <chandra.prakash@ensuesoft.com>
2026-02-02 01:24:26 +0530

    setup register api and other configrations

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,8 +2,8 @@
 
 // promises method
 
-const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
+export const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
